502eaae0b595cb98fb2ae5196e1afbed1fde8baa
Woody <colorguitar@hotmail.com>
2022-03-22 21:54:45 +0800

    group message and send to wecom

--- index.py
+++ index.py
@@ -1,8 +1,33 @@
 # -*- coding: utf8 -*-
 import json
+
 import requests
+
+template = '''
+    {
+    "msgtype": "markdown",
+    "markdown": {
+        "content": "%s"
+    }
+}
+'''
 
 
 def main_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     print("Received context: " + str(context))
+
+    event_list = event['EventList']
+    contents = ''
+    for single_event in event_list:
+        content = single_event['data']['markdown']['content']
+        contents += content + '\n\n'
+
+    print(contents)
+
+    data = template % contents
+
+    r = requests.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=81ef7bfa-0ff4-4850-86df-a8e272c8cafe',
+                      data=data.encode('utf-8'))
+    print(r.status_code)
+    print(r.content)
